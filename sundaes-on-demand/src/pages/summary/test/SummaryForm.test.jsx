b219313058147b3b3291897588f8126d5a647bee
maphnew <zcm3118@gmail.com>
2022-07-25 21:43:28 +0900

    Test and code summary form

---
+++ sundaes-on-demand/src/pages/summary/test/SummaryForm.test.jsx
@@ -0,0 +1,21 @@
+import { render, screen, fireEvent } from "@testing-library/react";
+import SummaryForm from "../SummaryForm";
+
+test("initial conditions", () => {
+  render(<SummaryForm />);
+  const checkbox = screen.getByRole("checkbox", { name: /terms and conditions/i });
+  expect(checkbox).not.toBeChecked();
+
+  const button = screen.getByRole("button", { name: /confirm order/i });
+  expect(button).toBeDisabled();
+});
+
+test("Checkbox enables button on first click and disables on second click", () => {
+  render(<SummaryForm />);
+  const button = screen.getByRole("button", { name: /confirm order/i });
+  const checkbox = screen.getByRole("checkbox", { name: /terms and conditions/i });
+  fireEvent.click(checkbox);
+  expect(button).toBeEnabled();
+  fireEvent.click(checkbox);
+  expect(button).toBeDisabled();
+});
