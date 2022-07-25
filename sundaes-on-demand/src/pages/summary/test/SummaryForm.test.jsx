4554c661ab8c2b264269f5b93409077481cdb639
maphnew <zcm3118@gmail.com>
2022-07-25 22:45:53 +0900

    user event and popover, async

--- sundaes-on-demand/src/pages/summary/test/SummaryForm.test.jsx
+++ sundaes-on-demand/src/pages/summary/test/SummaryForm.test.jsx
@@ -1,4 +1,4 @@
-import { render, screen } from "@testing-library/react";
+import { render, screen, waitForElementToBeRemoved } from "@testing-library/react";
 import SummaryForm from "../SummaryForm";
 import userEvent from "@testing-library/user-event";
 
@@ -30,11 +30,10 @@ test("popover responds to hover", async () => {
   expect(nullPopover).not.toBeInTheDocument();
   // popover appears upon mouseover of checkbox label
   const termsAndConditions = screen.getByText(/terms and conditions/i);
-  await user.hover(termsAndConditions);
+  user.hover(termsAndConditions);
   const popover = screen.getByText(/no ice cream will actually be delivered/i);
   expect(popover).toBeInTheDocument();
   // popover disappears when we mouse out
-  await user.unhover(termsAndConditions);
-  const nullPopoverAgain = screen.queryByText(/no ice cream will actually be delivered/i);
-  expect(nullPopoverAgain).not.toBeInTheDocument();
+  user.unhover(termsAndConditions);
+  await waitForElementToBeRemoved(() => screen.queryByText(/no ice cream will actually be delivered/i));
 });
