a321f31ccd1c747d72929ebb00c0f11ad159255c
maphnew <zcm3118@gmail.com>
2022-08-29 22:26:31 +0900

    69. Review of tests: scoop with context 1. getByText to find total   - exact option set to false 2. number inputs   - await and findBy   - spinbutton role   - userEvent.clear   - userEvent.type 3. wrapper option to render to apply context provider 4. Redefine Testing Library render to access universally

--- sundaes-on-demand/src/pages/entry/OrderEntry.jsx
+++ sundaes-on-demand/src/pages/entry/OrderEntry.jsx
@@ -3,8 +3,8 @@ import Options from "./Options";
 export default function OrderEntry() {
   return (
     <div>
-      <Options optionType={"scoops"} />
-      <Options optionType={"topping"} />
+      <Options optionType="scoops" />
+      <Options optionType="toppings" />
     </div>
   );
 }
